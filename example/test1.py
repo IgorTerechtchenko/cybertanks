49a48c0ec614ecc2f05b46271360b0ff6330b35a
shem-sergey <shem_sergey@tut.by>
2016-12-05 23:23:59 +0300

    Fixed PEP8 warnings.

--- example/test1.py
+++ example/test1.py
@@ -33,10 +33,10 @@ def main():
             if e.type == pygame.KEYDOWN:
                 move(e)
 
-        pygame.draw.rect(screen, pygame.Color("black"),
-                                 pygame.Rect(0, 0, screen.get_width(), screen.get_height()))
+        pygame.draw.rect(screen, pygame.Color("black"), pygame.Rect(0, 0, screen.get_width(), screen.get_height()))
         pygame.draw.rect(screen, red, pygame.Rect(x, y, 50, 50))
         pygame.display.flip()
+
 
 def recalc_x(x, v):
     if x + v <= 0:
@@ -46,6 +46,7 @@ def recalc_x(x, v):
     else:
         return x + v
 
+
 def recalc_vx(vx):
     if abs(vx) < FRICTION:
         return 0
@@ -54,13 +55,13 @@ def recalc_vx(vx):
     else:
         return vx + FRICTION
 
+
 def recalc_vy(y, v):
     if y >= screen.get_height() - 50 and v <= 0:
         return 0
-    # elif y >= screen.get_height() - 50 and v > 0:
-    #     return v
     else:
         return v - G
+
 
 def recalc_y(y, v):
     if (y - v) < 50:
@@ -69,6 +70,7 @@ def recalc_y(y, v):
         return screen.get_height() - 50
     else:
         return y - v
+
 
 def move(e):
     global x, y, vx, vy, G
