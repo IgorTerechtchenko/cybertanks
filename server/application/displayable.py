24944e7be54ff9b6580cdfd1fb974b367cbcb86a
Igor Tereschenko <pokawork@gmail.com>
2016-12-11 21:12:26 +0300

    massive fix

--- server/application/displayable.py
+++ server/application/displayable.py
@@ -1,8 +1,8 @@
-class Displayable(Object):
+class Displayable(object):
     """
     Base class for any object to be displayed on game field.
     object_name
-    
+
     get_name(self)
     intersects(self, another_displayable)
     display(self, another_displayable)
@@ -23,7 +23,7 @@ class Displayable(Object):
     # TODO: choose some particular display object type
     def display(self, display_object):
         """
-        Draws this object on display object (e.g. some 
+        Draws this object on display object (e.g. some
         drawing panel passed.)
         """
         return
@@ -31,10 +31,10 @@ class Displayable(Object):
     def get_shape(self):
         """Returns fully described shape of this object."""
         return
-    
+
     def get_coordinates(self):
         """
-        Returns tuple (x, y) containing coordinates of point 
+        Returns tuple (x, y) containing coordinates of point
         considered to be center of this object.
         """
         return
